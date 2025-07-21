f1cec025aa01fa93cc88a2e32671a6d503c2444e
krishna <krishnasai2801@gmail.com>
2025-07-21 08:21:54 +0530

    Asian Architect

---
+++ Gitiiii/src/seleniumone/complete7.java
@@ -0,0 +1,28 @@
+package selenium;
+
+public class complete {
+	
+	public void rest()
+	{
+		System.out.println("First");
+		
+		System.out.println("i am the first person here");
+		System.out.println("First");
+		System.out.println("First");
+	
+		
+	}
+	public void sleeps()
+	{
+		System.out.println("First");
+		System.out.println("First");
+		System.out.println("First");
+		System.out.println("First");
+		System.out.println("First");
+		
+		
+	
+		
+	}
+
+}
