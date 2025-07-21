996a7b7586f1e9bda1f7ddf5579d2a788fdc498f
krishna <krishnasai2801@gmail.com>
2025-07-21 08:15:37 +0530

    American architect

---
+++ Gitiiii/src/seleniumone/complete5.java
@@ -0,0 +1,27 @@
+package selenium;
+
+public class complete {
+
+	public void rest() {
+		System.out.println("Selenium");
+
+		System.out.println("i am the first person here");
+		System.out.println("Appium");
+
+		System.out.println("i am the second person here");
+
+	}
+
+	public void sleeps() {
+		System.out.println("First");
+
+		System.out.println("I am the second person here");
+		System.out.println("I am the third person here");
+		System.out.println("First");
+
+		System.out.println("I am the second person here");
+		System.out.println("I am the third person here");
+
+	}
+
+}
